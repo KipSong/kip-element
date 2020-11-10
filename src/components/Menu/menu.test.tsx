6e9eb17347280324359ddc0b67b7f33cd3115960
KipSong <kipsong@sina.com>
2020-11-10 19:15:00 +0800

    add

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
-import Menu, { MenuProps } from "./Menu"
+import { render, RenderResult, fireEvent, wait } from '@testing-library/react'
+import Menu, {MenuProps} from './Menu'
 import MenuItem from './MenuItem'
 import SubMenu from './SubMenu'
-import { render, fireEvent, RenderResult, cleanup, wait } from '@testing-library/react'
-
+jest.mock('../Icon/icon', () => {
+  return () => {
+    return <i className="fa" />
+  }
+})
+jest.mock('react-transition-group', () => {
+  return {
+    CSSTransition: (props: any) => {
+      return props.children
+    }
+  }
+})
 const testProps: MenuProps = {
-  defaultIndex: "0",
-  className: 'test',
-  onSelect: jest.fn()
+  defaultIndex: '0',
+  onSelect: jest.fn(),
+  className: 'test'
 }
-
-const testVerticalProps: MenuProps = {
-  defaultIndex: "0",
+const testVerProps: MenuProps = {
+  defaultIndex: '0',
   mode: 'vertical',
+  defaultOpenSubMenus: ['4']
 }
-
 const generateMenu = (props: MenuProps) => {
   return (
-    <>
     <Menu {...props}>
       <MenuItem>
         active
@@ -28,23 +37,26 @@ const generateMenu = (props: MenuProps) => {
       <MenuItem>
         xyz
       </MenuItem>
-      <SubMenu title='kipTitle'>
+      <SubMenu title="dropdown">
         <MenuItem>
-          subMenu1
+          drop1
+        </MenuItem>
+      </SubMenu>
+      <SubMenu title="opened">
+        <MenuItem>
+          opened1
         </MenuItem>
       </SubMenu>
     </Menu>
-    </>
   )
 }
-
 const createStyleFile = () => {
   const cssFile: string = `
     .kip-submenu {
       display: none;
     }
-    .kip-submenu.menu-opened{
-      display: block;
+    .kip-submenu.menu-opened {
+      display:block;
     }
   `
   const style = document.createElement('style')
@@ -52,61 +64,63 @@ const createStyleFile = () => {
   style.innerHTML = cssFile
   return style
 }
-let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement
-
-describe('test Menu and MenuItem based on default props', () => {
-  // 通用函数
+let wrapper: RenderResult, wrapper2: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement
+describe('test Menu and MenuItem component in default(horizontal) mode', () => {
   beforeEach(() => {
     wrapper = render(generateMenu(testProps))
-    menuElement = wrapper.getByTestId('test-menu')
     wrapper.container.append(createStyleFile())
-    //  wrapper.container // element
+    menuElement= wrapper.getByTestId('test-menu')
     activeElement = wrapper.getByText('active')
     disabledElement = wrapper.getByText('disabled')
-
   })
-  it('should render correct Menu and MenuItem base on default props', () => {
+  it('should render correct Menu and MenuItem based on default props', () => {
     expect(menuElement).toBeInTheDocument()
     expect(menuElement).toHaveClass('kip-menu test')
-    // expect(menuElement.getElementsByTagName('li').length).toEqual(3)  // 判断li 是否有3个
-    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4)
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(5)
     expect(activeElement).toHaveClass('menu-item is-active')
     expect(disabledElement).toHaveClass('menu-item is-disabled')
   })
-
-  it('click items shoud change active and call the right callback', () => {
+  it('click items should change active and call the right callback', () => {
     const thirdItem = wrapper.getByText('xyz')
     fireEvent.click(thirdItem)
-    expect(thirdItem).toHaveClass('menu-item is-active')  //点击元素是否存在active
-    expect(activeElement).not.toHaveClass('menu-item is-active')  // 选中元素当前没有 active
-    expect(testProps.onSelect).toHaveBeenCalledWith('2')  // 下标2
+    expect(thirdItem).toHaveClass('is-active')
+    expect(activeElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).toHaveBeenCalledWith('2')
     fireEvent.click(disabledElement)
-    expect(disabledElement).not.toHaveClass('is-active')    // 禁用的元素是否有active
-    expect(testProps.onSelect).not.toHaveBeenCalledWith('1')  // 下标1
+    expect(disabledElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1')
   })
-
+  it('should show dropdown items when hover on subMenu', async () => {
+    expect(wrapper.queryByText('drop1')).not.toBeVisible()
+    const dropdownElement = wrapper.getByText('dropdown')
+    fireEvent.mouseEnter(dropdownElement)
+    await wait(() => {
+      expect(wrapper.queryByText('drop1')).toBeVisible()
+    })
+    fireEvent.click(wrapper.getByText('drop1'))
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+    fireEvent.mouseLeave(dropdownElement)
+    await wait(() => {
+      expect(wrapper.queryByText('drop1')).not.toBeVisible()
+    })
+  })
+})
+describe('test Menu and MenuItem component in vertical mode', () => {
+  beforeEach(() => {
+    wrapper2 = render(generateMenu(testVerProps))
+    wrapper2.container.append(createStyleFile())
+  })
   it('should render vertical mode when mode is set to vertical', () => {
-    cleanup() // 清除之前的，否则这里est-menu 重复
-    const wrapper = render(generateMenu(testVerticalProps))
-    const menuElement = wrapper.getByTestId('test-menu')
+    const menuElement = wrapper2.getByTestId('test-menu')
     expect(menuElement).toHaveClass('menu-vertical')
   })
-
-  it('should show subMenu1 items when hover on subMenu', async () => {
-    expect(wrapper.queryByText('subMenu1')).not.toBeVisible()
-    const kipElement = wrapper.getByText('kipTitle')
-    fireEvent.mouseEnter(kipElement)
-    //  expect(wrapper.queryByText('subMenu1')).toBeVisible()  // 设定了 300毫秒 所以出错
-    await wait(() => {
-      expect(wrapper.queryByText('subMenu1')).toBeVisible()
-    })
-    fireEvent.click(wrapper.getByText('subMenu1'))
-    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
-
-    fireEvent.mouseLeave(kipElement)
-    await wait(() => {
-      expect(wrapper.queryByText('subMenu1')).not.toBeVisible()
-    })
+  it('should show dropdown items when click on subMenu for vertical mode', () => {
+    const dropDownItem = wrapper2.queryByText('drop1')
+    expect(dropDownItem).not.toBeVisible()
+    fireEvent.click(wrapper2.getByText('dropdown'))
+    expect(dropDownItem).toBeVisible()
   })
-
+  it('should show subMenu dropdown when defaultOpenSubMenus contains SubMenu index', () => {
+    expect(wrapper2.queryByText('opened1')).toBeVisible()
+  })
 })
